8b8db76a98d95a51ab3d569dbf71d849f782c610
Hari Narayanan <hnarayanan5@gmail.com>
2020-08-16 16:36:52 +0100

    Updates

---
+++ precache-manifest.443e06d0653ae65a28d8adefae27e611.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecf4d7d67e77630281fc97db5d4f5747",
+    "url": "/test/index.html"
+  },
+  {
+    "revision": "ba0fdf2826313700201c",
+    "url": "/test/static/css/main.cb05f6bb.chunk.css"
+  },
+  {
+    "revision": "e76a520727ad1c86408d",
+    "url": "/test/static/js/2.4de6fafc.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/test/static/js/2.4de6fafc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba0fdf2826313700201c",
+    "url": "/test/static/js/main.efc6f738.chunk.js"
+  },
+  {
+    "revision": "f560178fd10e904f4904",
+    "url": "/test/static/js/runtime-main.c78e0643.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/test/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "2bdb0d8d4aee3628a6602a8faad29e67",
+    "url": "/test/static/media/weather.2bdb0d8d.png"
+  }
+]);
